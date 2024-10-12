720cd515106be92c3606f11270085f3d7029edb1
why ega <egaaaaaaaaaaaaaaaaa@mail.ru>
2024-10-12 18:07:18 +0500

    фикс работы на трилогии (надеюсь работает)

--- plugin/src/main.cpp
+++ plugin/src/main.cpp
@@ -5,7 +5,8 @@ class main {
  public:
   main() {
 #ifdef TRILOGYSA
-    mywr::llmo::fill(0x10CC2F1, 0x90, 0x1F);
+    auto base_address = reinterpret_cast<uintptr_t>(GetModuleHandleA(NULL));
+    mywr::llmo::fill(base_address + 0x10CC2F1, 0x90, 0x1F);
 #else
     mywr::llmo::fill(0x60D89D, 0x90, 0x20);
 #endif
